4747ccc400936c376b1aada6a18e8fa039f2aa4d
Nandini <Nandini@TYSS-Nandini>
2024-07-12 15:56:51 +0530

    Commit First push

---
+++ SeleniumFramework_CRM/src/test/java/practice/InvoiceTest_Listeners.java
@@ -0,0 +1,25 @@
+package practice;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.crmcast.crm.generic.BaseUtility.BaseClass;
+//@Listeners(com.comcast.crm.listenerUtility.ListImpClass.class)
+public class InvoiceTest_Listeners extends BaseClass {
+	@Test
+	public void createInvoiceTest()  {
+		System.out.println("excute createInvoiceTest");
+		
+	String actTitle = driver.getTitle();
+	Assert.assertEquals(actTitle, "login");
+	System.out.println("steps after assert");
+	}
+
+	@Test
+	public void createInvoiceWithContactTest() {
+		System.out.println("Excute createInvoiceWithContactTest");
+		System.out.println("step-1");
+		System.out.println("step-2");
+
+	}
+}
